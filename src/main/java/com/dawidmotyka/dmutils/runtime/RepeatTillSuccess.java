2d6987f2328ab709ce0afbf527da714772096ae0
Dawid Motyka <dawidmt@gmail.com>
2020-12-20 14:55:34 +0100

    v1.2.0 * add "retry exceptions" to planTask * refactoring

--- src/main/java/com/dawidmotyka/dmutils/runtime/RepeatTillSuccess.java
+++ src/main/java/com/dawidmotyka/dmutils/runtime/RepeatTillSuccess.java
@@ -10,6 +10,7 @@
 
 package com.dawidmotyka.dmutils.runtime;
 
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -21,29 +22,33 @@ import java.util.concurrent.TimeUnit;
 public class RepeatTillSuccess {
     
     public interface RunnableWithException {
-        public void run() throws Exception;
+        void run() throws Exception;
     }
     public interface OnErrorListener {
-        public void onError(Exception t);
+        void onError(Exception t);
     }
     public interface OnDoneListener {
-        public void onDone();
+        void onDone();
     }
     public interface TaskFailedListener {
-        public void taskFailed();
+        void taskFailed();
     }
 
     private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-    private ScheduledFuture scheduledFuture = null;
+    private ScheduledFuture<?> scheduledFuture = null;
     private int numTries = 0;
 
     public void planTask(RunnableWithException task, OnDoneListener onDoneListener, OnErrorListener onErrorListener, int intervalMs, int maxRetries, TaskFailedListener taskFailedListener) {
+        planTask(task, onDoneListener, onErrorListener, intervalMs, maxRetries, taskFailedListener, null);
+    }
+
+    // notCountExceptions - when these exception types occurs running the task, this is not counted for retry limit
+    public void planTask(RunnableWithException task, OnDoneListener onDoneListener, OnErrorListener onErrorListener, int intervalMs, int maxRetries, TaskFailedListener taskFailedListener, Set<Class<? extends Exception>> notCountExceptions) {
         if (scheduledFuture != null)
             throw new IllegalStateException("Task already started");
-        int finalMaxRetries = maxRetries;
         scheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
-                if (finalMaxRetries > 0 && numTries >= finalMaxRetries) {
+                if (maxRetries > 0 && numTries >= maxRetries) {
                     taskFailedListener.taskFailed();
                     cancelTask();
                 } else {
@@ -54,6 +59,8 @@ public class RepeatTillSuccess {
                 }
             } catch (Exception e) {
                 onErrorListener.onError(e);
+                if (notCountExceptions != null && notCountExceptions.contains(e.getClass()))
+                    numTries -= 1;
             }
         }, 0, intervalMs, TimeUnit.MILLISECONDS);
     }
